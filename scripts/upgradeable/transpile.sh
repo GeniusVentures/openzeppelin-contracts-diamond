402037815b9e0ade6c2c298d7f018d7e11bc48c1
Kenneth Hurley <ken@geniusventures.io>
2022-02-07 20:27:45 -0800

    need to add contracts/utils/Escrow.sol public initializer for new call

--- scripts/upgradeable/transpile.sh
+++ scripts/upgradeable/transpile.sh
@@ -16,7 +16,8 @@ npx @gnus.ai/upgrade-safe-transpiler-diamond@latest -D -E \
   -x '!contracts/proxy/ERC1967/ERC1967{Storage,Upgrade}.sol' \
   -x '!contracts/proxy/utils/UUPSUpgradeable.sol' \
   -x '!contracts/proxy/beacon/IBeacon.sol' \
-  -p 'contracts/**/presets/**/*'
+  -p 'contracts/**/presets/**/*' \
+  -p 'contracts/utils/Escrow.sol'
 
 for p in scripts/upgradeable/patch/*.patch; do
   git apply "$p"
